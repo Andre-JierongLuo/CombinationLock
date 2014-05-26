e3308b451448565fc14eab172b3536cb655265c3
Andre <rjluo17@gmail.com>
2014-05-26 12:13:23 -0400

    Delete comboLock.java

--- comboLock.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-public class comboLock{
-	public static void main(String[] args){
-		JFrame f = new JFrame("Combo Lock");
-		f.setSize(300,400);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setVisible(true);
-	}
-}
