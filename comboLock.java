78ea19065a25e4049ebfd67c01571651a0e536c2
Andre-JierongLuo <rjluo17@gmail.com>
2014-05-26 11:15:35 -0400

    setup

---
+++ comboLock.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class comboLock{
+	public static void main(String[] args){
+		JFrame f = new JFrame("Combo Lock");
+		f.setSize(300,400);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+}
